71512b90c4f6ca4774dcf433e0e10d254482aa8b
abdur-rehman-ali <alijoyia911@gmail.com>
2024-07-12 17:10:11 -0700

    Created auth service

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import './App.css'
-import { conf } from './conf/conf'
 
 function App() {
   return (
     <>
-      <ul>
-        <li>{ conf.appwriteURL }</li>
-        <li>{ conf.appwriteProjectID }</li>
-        <li>{ conf.appwriteDatabaseID }</li>
-        <li>{ conf.appwriteBlogsCollectionID }</li>
-        <li>{ conf.appwriteBucketID }</li>
-      </ul>
+      <h1>Welcome to blogs app</h1>
     </>
   )
 }
---
+++ src/services/auth.service.js
@@ -0,0 +1,58 @@
+import { conf } from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteURL)
+      .setProject(conf.appwriteProjectID);
+    this.account = new Account(this.client);
+  }
+
+  async registerUser({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create
+        (ID.unique(), email, password, name)
+      if (userAccount) {
+        this.loginUser(email, password)
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async loginUser({ email, password }) {
+    try {
+      return await this.account
+        .createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.error("getCurrentUser :: Error", error.message);
+    }
+    return null;
+  }
+
+  async logoutUser() {
+    try {
+      return await this.account.deleteSession('current');
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
