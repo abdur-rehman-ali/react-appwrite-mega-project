0296d2fb417703860889143c6b298f74fadda29c
abdur-rehman-ali <alijoyia911@gmail.com>
2024-07-16 16:28:52 -0700

    Add navigation and footer

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/footer/Footer'
 
 const RootLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   )
 }
--- src/pages/LoginUser.jsx
+++ src/pages/LoginUser.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Input from '../components/shared/Input'
+import { Link } from 'react-router-dom'
 
 const LoginUser = () => {
   return (
@@ -32,7 +33,7 @@ const LoginUser = () => {
 
               <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login here</button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don't have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Create here</a>
+                Don't have an account? <Link to="/accounts/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Create here</Link>
               </p>
             </form>
           </div>
