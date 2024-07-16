8efe2cf7156b85ce0192765a83e98c7709f7c2e2
abdur-rehman-ali <alijoyia911@gmail.com>
2024-07-16 16:24:42 -0700

    Add react router dom and added pages

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -9,18 +10,13 @@ const Navbar = () => {
       isAuthenticated: true,
     },
     {
-      title: "About",
-      url: "/about",
-      isAuthenticated: false,
-    },
-    {
-      title: "Sevices",
-      url: "/services",
+      title: "Register",
+      url: "/accounts/register",
       isAuthenticated: true,
     },
     {
-      title: "Contact Us",
-      url: "/contact-us",
+      title: "Login",
+      url: "/accounts/login",
       isAuthenticated: true,
     },
   ]
@@ -46,7 +42,7 @@ const Navbar = () => {
             {
               Array.isArray(navigationLinks) && navigationLinks.map((navigationLink, index) => (
                 navigationLink.isAuthenticated && (<li key={index}>
-                  <a href={`${navigationLink.url}`} className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{navigationLink.title}</a>
+                  <Link to={`${navigationLink.url}`} className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{navigationLink.title}</Link>
                 </li>)
               ))
             }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements
+} from "react-router-dom";
+import RootLayout from './layouts/RootLayout.jsx';
+import LoginUser from './pages/LoginUser.jsx';
+import RegisterUser from './pages/RegisterUser.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route path="/accounts/login" element={<LoginUser />} />
+      <Route path="/accounts/register" element={<RegisterUser />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
